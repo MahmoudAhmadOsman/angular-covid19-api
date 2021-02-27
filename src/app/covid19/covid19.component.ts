6dd231323d58d14ebb267da080f24137f371c44c
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-02-26 23:16:16 -0600

    added error handling pipe

--- src/app/covid19/covid19.component.ts
+++ src/app/covid19/covid19.component.ts
@@ -1,6 +1,8 @@
 import { Covid19Service } from './../services/covid19.service';
 import { Component, OnInit } from '@angular/core';
-
+// import "rxjs/add/operator/catch";
+// import "rxjs/add/Observable/throw";
+// import { Observable, throwError } from 'rxjs';
 @Component({
   selector: 'app-covid19',
   templateUrl: './covid19.component.html',
@@ -14,25 +16,32 @@ public CountryName:any = '';
   //Inject the covid Service here
     countries: any[];
     totalRecords: String;
-    page: Number=1;
+  page: Number = 1;
+  
+ //error handling
+  public errorMessage: String ="Loading...";
+  
   constructor(covidService: Covid19Service) {
+
+
     //subscrbe(res, err) -> takes res and err
-  // 
     covidService.getCovid().subscribe(
       (data) => {
-        console.log(data);
-  // this.data = new Array<any>()
+        //console.log(data);
         //set the res to the countries array
         this.countries = data;
 
-//countries = result
+      //countries = result
         //Total records
       this.totalRecords = data.countries.length;
       //console.log("Total Records: ", totalRecords);
 
-      },
-      (err) => {
-        console.log("error", err);
+      },(error) => {
+        console.log("There is an error: ", error);
+       // this.errorMessage = error;
+     this.errorMessage = "Error occured while making an api call";
+         
+ 
       }
     );
   }
--- src/app/services/covid19.service.ts
+++ src/app/services/covid19.service.ts
@@ -1,23 +1,33 @@
+import { catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+ 
 
 @Injectable({
   providedIn: 'root'
 })
 export class Covid19Service {
 
-  constructor(private htpp: HttpClient) {
-    
-  }
+  constructor(private htpp: HttpClient) {}
 
   //Create custom function
   getCovid(): Observable<any> {
-    //https://jsonplaceholder.typicode.com/posts
+ 
     //https://api.covid19api.com/countries
+    // const URL = "https://corona.lmao.ninja/v3/covid-19/countries";
+    // return this.htpp.get<any>(URL)
     const URL = "https://corona.lmao.ninja/v3/covid-19/countries";
-    return this.htpp.get(URL);
+    return this.htpp.get<any>(URL)
+      .pipe(catchError(this.handleError))
+   
   }
+
+  handleError(error) {
+    return throwError(error.message || "Server Error!")
+  }
+
+
 }
 
 
