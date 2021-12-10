c786f407c54b91b32095ca45cb51499acd05f2d6
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-12-09 21:22:04 -0600

    added ng-template

--- src/app/services/covid19.service.ts
+++ src/app/services/covid19.service.ts
@@ -2,25 +2,20 @@ import { catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
- 
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Covid19Service {
-
   constructor(private htpp: HttpClient) {}
 
   //Create custom function
   getCovid(): Observable<any> {
-
-    const URL = "https://corona.lmao.ninja/v3/covid-19/countries";
-    return this.htpp.get<any>(URL)
-      .pipe(catchError(this.handleError))
-   
+    const URL = 'https://corona.lmao.ninja/v3/covid-19/countries';
+    return this.htpp.get<any>(URL).pipe(catchError(this.handleError));
   }
 
   handleError(error) {
-    return throwError(error.message || "Server Error!")
+    return throwError(error.message || 'Cannot fetch data!');
   }
 }
